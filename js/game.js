4c5a69d3d2d3f0d68a221944928b798c1037eb51
matukai <matt.tanji@gmail.com>
2018-01-25 19:24:26 -1000

    movement fixed

--- js/game.js
+++ js/game.js
@@ -4,27 +4,63 @@
   const GAME_HEIGHT = 600;
   const GAME_CONTAINER_ID = 'game'
   const GFX = 'gfx';
-  
-  const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update});
+  const INITIAL_MOVESPEED = 4;
+
+  const SQRT_TWO = Math.sqrt(2);
+  const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, {
+    preload,
+    create,
+    update
+  });
 
   let player;
-
+  let cursors
+  let playerBullets;
 
   //load assets | core game methods
-  function preload(){
+  function preload() {
     game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
   };
 
-  //
   function create() {
-    player = game.add.sprite(200, 400, GFX, 8);
+    cursors = game.input.keyboard.createCursorKeys();
+    player = game.add.sprite(100, 100, GFX, 8);
+    player.moveSpeed = INITIAL_MOVESPEED;
+    playerBullets = game.add.group();
   };
 
   //runs every 60 secs
   function update() {
-
+    handlePlayerMovement();
   };
 
+  //handler function 
+  function handlePlayerMovement() {
+    let movingH = SQRT_TWO;
+    let movingV = SQRT_TWO;
+    if (cursors.up.isDown || cursors.down.isDown) {
+      movingH = 1; // slow down diagonal movement
+    }
+    if (cursors.left.isDown || cursors.right.isDown) {
+      movingV = 1; // slow down diagonal movement
+    }
+    switch (true) {
+      case cursors.left.isDown:
+        player.x -= player.moveSpeed;
+        break;
+      case cursors.right.isDown:
+        player.x += player.moveSpeed;
+        break;
+    }
+    switch (true) {
+      case cursors.down.isDown:
+        player.y += player.moveSpeed;
+        break;
+      case cursors.up.isDown:
+        player.y -= player.moveSpeed;
+        break;
+    }
+  };
 
 
 
